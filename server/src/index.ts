3e406f4879d69ef7b6967635a3914ada2e5c5366
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-19 13:53:38 -0400

    feat: integrate post routes into the main server file

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,18 +2,16 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/post.routes';
 
-// 1. Configuración inicial
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-// 2. Middlewares (Los porteros de la discoteca)
-app.use(cors()); // Permite peticiones desde el Frontend
-app.use(express.json()); // Permite leer JSON en el body de las peticiones
+app.use(cors());
+app.use(express.json());
 
-// 3. Conexión a Base de Datos (MongoDB Atlas)
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI as string);
@@ -24,12 +22,13 @@ const connectDB = async () => {
   }
 };
 
-// 4. Rutas de prueba
 app.get('/', (req: Request, res: Response) => {
   res.send('API de LinkedIn Clone funcionando 🚀');
 });
 
-// 5. Encender el servidor
+// 2. USAR LA RUTA (Todo lo que vaya a /api/posts será manejado por postRoutes)
+app.use('/api/posts', postRoutes); 
+
 app.listen(port, () => {
   connectDB();
   console.log(`⚡ Servidor corriendo en http://localhost:${port}`);
